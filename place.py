9930e08a2c0b8b1636fb1ec7d1c37f6fbe275fa1
imjustlazy <2628465713@qq.com>
2020-05-15 22:28:40 +0800

    finished map description

--- place.py
+++ place.py
@@ -3,7 +3,12 @@
 class Place(object):
     name = ''
     description = ''
+    pre = ''
+    sub = ''
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    
+    def add_effect(self):
+        pass
